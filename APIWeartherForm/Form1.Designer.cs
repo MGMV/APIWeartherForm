db81613f97e4490d8eb78880ea2133e3fea14573
budik <budik.v@yandex.ru>
2021-02-25 11:22:59 +0300

    Добавьте файлы проекта.

---
+++ APIWeartherForm/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace APIWeartherForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Temptxt = new System.Windows.Forms.Label();
+            this.feelsLikeTxt = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.Info;
+            this.groupBox1.Controls.Add(this.feelsLikeTxt);
+            this.groupBox1.Controls.Add(this.Temptxt);
+            this.groupBox1.Location = new System.Drawing.Point(359, 57);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(144, 184);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Город";
+            // 
+            // Temptxt
+            // 
+            this.Temptxt.AutoSize = true;
+            this.Temptxt.Location = new System.Drawing.Point(6, 28);
+            this.Temptxt.Name = "Temptxt";
+            this.Temptxt.Size = new System.Drawing.Size(80, 13);
+            this.Temptxt.TabIndex = 0;
+            this.Temptxt.Text = "Температура: ";
+            // 
+            // feelsLikeTxt
+            // 
+            this.feelsLikeTxt.AutoSize = true;
+            this.feelsLikeTxt.Location = new System.Drawing.Point(6, 41);
+            this.feelsLikeTxt.Name = "feelsLikeTxt";
+            this.feelsLikeTxt.Size = new System.Drawing.Size(73, 13);
+            this.feelsLikeTxt.TabIndex = 0;
+            this.feelsLikeTxt.Text = "Ощущается: ";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(539, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(249, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.TextUpdate += new System.EventHandler(this.comboBox1_TextUpdate);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Погода";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label feelsLikeTxt;
+        private System.Windows.Forms.Label Temptxt;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ APIWeartherForm/Form1.cs
@@ -0,0 +1,106 @@
+﻿using Dadata;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace APIWeartherForm
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void weather(Geo geo)
+        {
+            string url = @"https://api.weather.yandex.ru/v2/forecast?";
+            url += "lat=" + geo.geo_lat;
+            url += "&lon=" + geo.geo_lon;
+            groupBox1.Name = geo.name;
+
+
+            WebRequest webRequest = WebRequest.Create(url);
+            webRequest.Headers.Add("X-Yandex-API-Key:53784e43-078c-4410-b236-190616cd4a9e");
+            WebResponse webResponse = webRequest.GetResponse();
+            Stream stream = webResponse.GetResponseStream();
+            StreamReader sr = new StreamReader(stream);
+            string s = sr.ReadToEnd();
+            var sss = JObject.Parse(s);
+
+            string name = sss["geo_object"]["locality"]["name"].ToString();
+            int temp = Int32.Parse(sss["fact"]["temp"].ToString());
+            int feels_like = Int32.Parse(sss["fact"]["feels_like"].ToString());
+
+            groupBox1.Text = name;
+
+            Temptxt.Text = "Температура: " + temp.ToString();
+            feelsLikeTxt.Text = "Ощущается: " + feels_like.ToString();
+        }
+
+        private async Task asdAsync()
+        {
+
+            // https://github.com/hflabs/dadata-csharp
+
+            var token = "8550db05a079b0fbcb6468f69798b93896aa7887";
+            var api = new SuggestClientAsync(token);
+            var result = await api.SuggestAddress(comboBox1.Text);
+            string str = "";
+            comboBox1.Items.Add(result.suggestions[0].value);
+            if (comboBox1.Items.Count > 1)
+                comboBox1.Items.RemoveAt(0);
+
+        }
+
+        private async Task<Geo> asdAsyncGeo()
+        {
+
+            // https://github.com/hflabs/dadata-csharp
+
+            var token = "8550db05a079b0fbcb6468f69798b93896aa7887";
+            var api = new SuggestClientAsync(token);
+            var result = await api.SuggestAddress(comboBox1.Text);
+            string str = "";
+            Geo geo = new Geo();
+            geo.name = result.suggestions[0].value;
+            geo.geo_lon = result.suggestions[0].data.geo_lon;
+            geo.geo_lat = result.suggestions[0].data.geo_lat;
+            return geo;
+        }
+
+        private void comboBox1_TextUpdate(object sender, EventArgs e)
+        {
+            asdAsync();
+        }
+
+        private async void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Geo geo = await asdAsyncGeo();
+            weather(geo);
+
+        }
+    }
+    public class Geo
+    { 
+        public string name { get; set; }
+        public string geo_lat { get; set; }
+        public string geo_lon { get; set; }
+    }
+}
